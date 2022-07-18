12612cd7d777b951904619d56b5dde72422d776d
Jniko <nikolaisenmail@gmail.com>
2022-07-18 11:44:01 +0200

    Første arbeid på nettbutikk

---
+++ src/Components/Category-directory/CategoriesDirectoryFiles.js
@@ -0,0 +1,15 @@
+import CategoryItem from "../Category-item/category-item.component"
+import "./Directory.styles.scss"
+
+
+const Directory = ({categories}) => {
+    return(
+    <div className="categories-container">
+    {categories.map((category) => (
+      <CategoryItem key={category.id} category={category}/>
+    ))}
+  
+</div>
+    )
+}
+export default Directory;
