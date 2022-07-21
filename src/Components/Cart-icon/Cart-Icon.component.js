e52cca202a120179c6fa1881e0d1890967caeabe
Jniko <nikolaisenmail@gmail.com>
2022-07-21 11:32:11 +0200

    Added Cart fixes

--- src/Components/Cart-icon/Cart-Icon.component.js
+++ src/Components/Cart-icon/Cart-Icon.component.js
@@ -5,13 +5,13 @@ import { CartContext } from "../../Contexts/cart.context";
 
 const CartIcon = () => {
 
-    const {isCartOpen, setIsCartOpen} = useContext(CartContext);
+    const {isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
     return(
         <section className="cart-icon-container" onClick={toggleIsCartOpen}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">0</span>
+            <span className="item-count">{cartCount}</span>
         </section>
     )
 }
--- src/Components/Cart-item/Cart-item.component.js
+++ src/Components/Cart-item/Cart-item.component.js
@@ -3,11 +3,18 @@ import "./Cart-item.styles.scss"
 
 
 const CartItem = ({cartItem}) => {
-    const {name, quantity} = cartItem;
+    const {name, quantity, imageUrl, price,} = cartItem;
     return(
-        <section>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+        <section className="cart-item-container">
+            <img src={imageUrl} alt={`${name}`} />
+            
+            <article className="item-details">
+                <span className="name">{name}</span>
+                <span className="price">
+                    {quantity} x ${price}
+                    </span>
+            </article>
+            
         </section>
     )
 }
